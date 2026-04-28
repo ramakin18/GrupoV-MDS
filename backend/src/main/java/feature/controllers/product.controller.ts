b8c0a369182de0e572e61cbdd005530916330cff
Ramiro-bit <ramirancogonzalez@gmail.com>
2026-04-28 19:35:16 -0300

    Controlador creado.
    1. Se creo el controlador con la lógica de validación de datos y captura de errores por parte del service.

---
+++ backend/src/main/java/feature/controllers/product.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { ProductServiceImpl } from '../services/impl/domain/product.service.impl';
+
+const productService = new ProductServiceImpl();
+
+export const getAll = (_req: Request, res: Response) => {
+    const products = productService.listAll();
+    return res.json(products);
+};
+
+export const create = (req: Request, res: Response) => {
+    try {
+        const { sku, description, price, stock } = req.body;
+
+        // Validamos el SKU y la descripción como datos obligatorios.
+        if (!sku || !description) {
+            return res.status(400).json({ message: "El SKU y la descripción son obligatorios." });
+        }
+        // Validamos que todo precio debe ser igual o mayor a 0 (igual por si el usuario quisiera hacer alguna prueba u oferta).
+        if (price < 0) {
+            return res.status(400).json({ message: "El precio debe ser 0 o un valor positivo." });
+        }
+        // Validamos que el sea mayor o igual a 0.
+        if (stock < 0) {
+            return res.status(400).json({ message: "El stock no puede ser negativo." });
+        }
+
+        const result = productService.create(req.body);
+        // Respuesta exitosa.
+        return res.status(201).json(result);
+
+    } catch (error: any) {
+        // Captura de errores por parte del service.
+        return res.status(409).json({ message: error.message });
+    }
+};
